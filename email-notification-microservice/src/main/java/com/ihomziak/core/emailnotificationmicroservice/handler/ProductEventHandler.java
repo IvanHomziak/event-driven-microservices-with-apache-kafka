9adf46809c917c86b811ac577a524c72cc04c9c0
ft34iv <ivan.homziak@ing.com>
2025-08-21 23:04:01 +0200

    Added kafka config

--- email-notification-microservice/src/main/java/com/ihomziak/core/emailnotificationmicroservice/handler/ProductEventHandler.java
+++ email-notification-microservice/src/main/java/com/ihomziak/core/emailnotificationmicroservice/handler/ProductEventHandler.java
@@ -1,17 +1,19 @@
 package com.ihomziak.core.emailnotificationmicroservice.handler;
 
 import com.ihomziak.core.core.ProductCreatedEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @KafkaListener(topics = "product-created-events-topic")
 public class ProductEventHandler {
 
-    private final Logger log = LoggerFactory.getLogger(ProductEventHandler.class);
+//    private final Logger log = LoggerFactory.getLogger(ProductEventHandler.class);
 
     @KafkaHandler
     public void handle(ProductCreatedEvent productCreatedEvent) {
