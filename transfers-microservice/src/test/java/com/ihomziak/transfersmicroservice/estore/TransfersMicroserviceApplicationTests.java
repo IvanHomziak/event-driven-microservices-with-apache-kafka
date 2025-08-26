c494f4ad8bd4c799cb78269ca5141c2814012894
IvanHomziak <ivan.homziak@gmail.com>
2025-08-26 22:53:31 +0200

    added deposit-microservice

---
+++ transfers-microservice/src/test/java/com/ihomziak/transfersmicroservice/estore/TransfersMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihomziak.transfersmicroservice.estore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfersMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
