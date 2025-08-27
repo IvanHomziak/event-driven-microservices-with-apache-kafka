6ee71f200b773094694b49fca4c623378063a69d
IvanHomziak <ivan.homziak@gmail.com>
2025-08-27 15:30:54 +0200

    ref

--- transfers-microservice/src/main/java/com/ihomziak/transfersmicroservice/estore/TransfersMicroserviceApplication.java
+++ transfers-microservice/src/main/java/com/ihomziak/transfersmicroservice/estore/TransfersMicroserviceApplication.java
@@ -2,6 +2,8 @@ package com.ihomziak.transfersmicroservice.estore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class TransfersMicroserviceApplication {
@@ -10,4 +12,8 @@ public class TransfersMicroserviceApplication {
         SpringApplication.run(TransfersMicroserviceApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
